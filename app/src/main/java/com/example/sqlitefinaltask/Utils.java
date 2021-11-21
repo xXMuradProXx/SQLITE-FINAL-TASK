3355f7a94c49d63a18990b91c4b86921ba1c014e
MuradTheBest <muradragimli@gmai.com>
2021-11-21 13:53:25 +0200

    for itai

--- app/src/main/java/com/example/sqlitefinaltask/Utils.java
+++ app/src/main/java/com/example/sqlitefinaltask/Utils.java
@@ -1,5 +1,7 @@
 package com.example.sqlitefinaltask;
 
+import android.database.sqlite.SQLiteDatabase;
+
 public class Utils {
     final static String TABLE_STUDENT_NAME = "tbl_student";
 
@@ -7,7 +9,6 @@ public class Utils {
     final static String TABLE_STUDENT_COL_NAME = "student_name";
     final static String TABLE_STUDENT_COL_SURNAME = "student_surname";
     final static String TABLE_STUDENT_COL_AVERAGE = "student_average";
-
 
     final static String TABLE_CLASS_NAME = "tbl_class";
     final static String TABLE_CLASS_COL_ID = "class_id";
@@ -20,7 +21,17 @@ public class Utils {
     final static String TABLE_TEACHER_COL_SURNAME = "teacher_surname";
     final static String TABLE_TEACHER_COL_PROFESSION = "teacher_profession";
 
-    public
+    public static void createAllTables(SQLiteDatabase db){
+        db.execSQL("create table if not exists" +
+                " tbl_student(student_id integer primary key autoincrement, student_name text, student_surname text, student_average integer)");
 
+        db.execSQL("create table if not exists" +
+                " tbl_class(class_id integer primary key autoincrement, class_name integer, class_teacher text)");
+
+        db.execSQL("create table if not exists" +
+                " tbl_teacher(teacher_id integer primary key autoincrement, teacher_name text, teacher_surname text, teacher_profession text)");
+    }
+
+    public class void
 
 }
